d3501653251609442bf4a868c34fc8f3e5b54ea0
Jetalir <Jetalir@hotmail.com>
2024-09-21 21:15:49 +0200

    Basic versionen enligt hemuppgiften

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace BankAccount
@@ -9,10 +10,41 @@ namespace BankAccount
     public class BankAccount
     {
         //Lägg till Egenskaper (fields)
+        private string AccountNumber { get; set; } // känslig info så den ska vara privat
+        public string AccountHolder { get; set; }
+        private double Balance { get; set; } // känslig info så den ska vara privat
 
         //Lägg till Konstruktor
-
+        public BankAccount(string accountnumber, String accountholder, double balance) 
+        {
+            AccountNumber = accountnumber;
+            AccountHolder = accountholder;
+            Balance = balance;
+        }
         //Lägg till Metoder
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Deposited: {amount}, New Balance: {Balance += amount}");
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (Balance < amount)
+            {
+                Console.WriteLine("You didn't have enough balance to Withdraw!");
+            }
+            else
+            {
+                Console.WriteLine($"Withdrew: {amount}, New Balance: {Balance -= amount}");
+            }
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"Balance: {Balance}"); 
+        }
+
+
 
         //Lycka till! :)
     }
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             //Instansiera bankAccount klassen och testa den
+            BankAccount account = new BankAccount("123456789", "Jetlir Kuci", 1000);
+            account.Deposit(500);
+            account.Withdraw(200);
+            account.DisplayBalance();
         }
     }
 }
